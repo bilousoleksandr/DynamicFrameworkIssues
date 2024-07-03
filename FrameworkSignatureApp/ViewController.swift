3f20a54096dd5b8db0ff227bfa9f94a2b72105b4
Oleksandr Bilous <oleksandrbilous@Oleksandrs-MacBook-Pro.local>
2024-07-04 00:00:34 +0300

    feat: project

---
+++ FrameworkSignatureApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FrameworkSignatureApp
+//
+//  Created by Oleksandr Bilous on 03.07.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
